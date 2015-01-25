298e11212c0288c0caadd1a040026cb5c9324047
merlos <merlos@gmail.com>
2015-01-25 04:02:06 +0100

    fixed a few typos

--- OpenGpxTracker/GPXTileServer.swift
+++ OpenGpxTracker/GPXTileServer.swift
@@ -10,16 +10,16 @@ import Foundation
 
 enum GPXTileServer {
     case Apple
-    case OpenStreetMaps
-    case MapBox
+    case OpenStreetMap
+    case MapQuest
     case OpenCycleMap
     //case AnotherMap
     
     var name: String {
         switch self {
         case .Apple: return "Apple Mapkit"
-        case .OpenStreetMaps: return "Open Street Maps"
-        case .MapBox: return "MapBox"
+        case .OpenStreetMaps: return "Open Street Map"
+        case .MapQuest: return "MapQuest Open"
         case .OpenCycleMap: return "Open Cycle Maps"
         //case .AnotherMap: return "My Map"
         }
@@ -27,8 +27,8 @@ enum GPXTileServer {
     var templateUrl: String {
         switch self {
         case .Apple: return ""
-        case .OpenStreetMaps: return "http://tile.openstreetmap.org/{z}/{x}/{y}.png"
-        case .MapBox: return "http://otile3.mqcdn.com/tiles/1.0.0/osm/{z}/{x}/{y}.jpg"
+        case .OpenStreetMap: return "http://tile.openstreetmap.org/{z}/{x}/{y}.png"
+        case .MapQuest: return "http://otile3.mqcdn.com/tiles/1.0.0/osm/{z}/{x}/{y}.jpg"
         case .OpenCycleMap: return "http://b.tile.opencyclemap.org/cycle/{z}/{x}/{y}.png"
         //case .AnotherMap: return "http://another.map.tile.server/{z}/{x}/{y}.png"
         }
