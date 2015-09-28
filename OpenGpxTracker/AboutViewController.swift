23e655700d2f1acd82ad856523a40fc504938d5a
merlos <merlos@gmail.com>
2015-09-28 03:30:28 +0200

    about to ios9

--- OpenGpxTracker/AboutViewController.swift
+++ OpenGpxTracker/AboutViewController.swift
@@ -13,7 +13,7 @@ class AboutViewController : UIViewController, UIWebViewDelegate {
     
     var webView : UIWebView?
     
-    required init(coder aDecoder: NSCoder) {
+    required init?(coder aDecoder: NSCoder) {
         super.init(coder: aDecoder)
     }
 
@@ -30,24 +30,24 @@ class AboutViewController : UIViewController, UIWebViewDelegate {
         self.title = "About"
         
         //Add the done button
-        let shareItem = UIBarButtonItem(title: "Done", style: UIBarButtonItemStyle.Bordered, target: self, action: "closeViewController")
+        let shareItem = UIBarButtonItem(title: "Done", style: UIBarButtonItemStyle.Plain, target: self, action: "closeViewController")
         self.navigationItem.rightBarButtonItems = [shareItem]
   
         //Add the Webview
         self.webView = UIWebView(frame: self.view.frame)
         self.webView?.delegate = self
-        var path = NSBundle.mainBundle().pathForResource("about", ofType: "html")
-        let text = String(contentsOfFile: path!, encoding: NSUTF8StringEncoding, error: nil);
+        let path = NSBundle.mainBundle().pathForResource("about", ofType: "html")
+        let text = try? String(contentsOfFile: path!, encoding: NSUTF8StringEncoding);
         
-        webView?.loadHTMLString(text, baseURL: nil)
+        webView?.loadHTMLString(text!, baseURL: nil)
         self.view.addSubview(webView!)
     }
     
     func webView(webView: UIWebView, shouldStartLoadWithRequest request: NSURLRequest, navigationType: UIWebViewNavigationType) -> Bool {
-        println("shouldStartLoadWithRequest")
+        print("shouldStartLoadWithRequest")
         
         if navigationType == UIWebViewNavigationType.LinkClicked {
-            UIApplication.sharedApplication().openURL(request.URL)
+            UIApplication.sharedApplication().openURL(request.URL!)
             return false
         }
         return true
