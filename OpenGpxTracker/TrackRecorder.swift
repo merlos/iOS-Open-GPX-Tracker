36609d875b51fe0235161b684b5252b03ed9cc19
merlos <merlos@gmail.com>
2016-07-20 02:54:44 +0200

    track reorder updated with @onato code

--- OpenGpxTracker/TrackRecorder.swift
+++ OpenGpxTracker/TrackRecorder.swift
@@ -1,9 +1,53 @@
-//
-//  TrackRecorder.swift
-//  OpenGpxTracker
-//
-//  Created by merlos on 20/07/16.
-//  Copyright © 2016 TransitBox. All rights reserved.
-//
+import CoreLocation
 
-import Foundation
+public protocol TrackRecorderDelegate: NSObjectProtocol {
+    func trackRecorder(recorder: TrackRecorder, didUpdateToLocation newLocation: CLLocation)
+}
+
+public class TrackRecorder: NSObject, CLLocationManagerDelegate {
+    weak var delegate: TrackRecorderDelegate?
+    
+    public var currentCoordinate: CLLocationCoordinate2D {
+        get {
+            return locationManager.location?.coordinate ?? CLLocationCoordinate2D(latitude: 8.90, longitude: -79.50)
+        }
+    }
+    
+    let locationManager: CLLocationManager = {
+        let manager = CLLocationManager()
+        manager.requestAlwaysAuthorization()
+        
+        manager.desiredAccuracy = kCLLocationAccuracyBest
+        manager.distanceFilter = 2
+        manager.pausesLocationUpdatesAutomatically = false
+        if #available(iOS 9.0, *) {
+            manager.allowsBackgroundLocationUpdates = true
+        }
+        return manager
+    }()
+    
+    override init() {
+        super.init()
+        locationManager.delegate = self
+        locationManager.startUpdatingLocation()
+    }
+    
+    public func start() {
+        locationManager.startUpdatingLocation()
+    }
+    
+    public func stop() {
+        locationManager.stopUpdatingLocation()
+    }
+    
+    public func locationManager(manager: CLLocationManager, didFailWithError error: NSError) {
+        print("didFailWithError\(error)")
+    }
+    
+    public func locationManager(manager: CLLocationManager, didUpdateToLocation newLocation: CLLocation, fromLocation oldLocation: CLLocation) {
+        //        print("didUpdateToLocation \(newLocation.coordinate.latitude),\(newLocation.coordinate.longitude),",
+        //            "Hacc: \(newLocation.horizontalAccuracy), Vacc: \(newLocation.verticalAccuracy)")
+        
+        delegate?.trackRecorder(self, didUpdateToLocation: newLocation)
+    }
+}
