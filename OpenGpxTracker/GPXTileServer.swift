d1a37a16b1482367168f1ce0a110e0d4132e7485
merlos <merlos@gmail.com>
2016-07-20 02:55:54 +0200

    + removed MapQuest + added TrackRecorder to project file (project.pbxproj)

--- OpenGpxTracker/GPXTileServer.swift
+++ OpenGpxTracker/GPXTileServer.swift
@@ -11,7 +11,7 @@ import Foundation
 enum GPXTileServer: Int {
     case Apple
     case OpenStreetMap
-    case MapQuest
+    //case MapQuest
     case OpenCycleMap
     //case AnotherMap
     case CartoDB
@@ -21,7 +21,7 @@ enum GPXTileServer: Int {
         switch self {
         case .Apple: return "Apple Mapkit"
         case .OpenStreetMap: return "Open Street Map"
-        case .MapQuest: return "MapQuest Open"
+        //case .MapQuest: return "MapQuest Open"
         case .OpenCycleMap: return "Open Cycle Maps"
         case .CartoDB: return "Carto DB"
         //case .AnotherMap: return "My Map"
@@ -32,7 +32,7 @@ enum GPXTileServer: Int {
         switch self {
         case .Apple: return ""
         case .OpenStreetMap: return "http://tile.openstreetmap.org/{z}/{x}/{y}.png"
-        case .MapQuest: return "http://otile3.mqcdn.com/tiles/1.0.0/osm/{z}/{x}/{y}.jpg"
+        //case .MapQuest: return "http://otile3.mqcdn.com/tiles/1.0.0/osm/{z}/{x}/{y}.jpg"
         case .OpenCycleMap: return "http://b.tile.opencyclemap.org/cycle/{z}/{x}/{y}.png"
         case .CartoDB: return "http://b.basemaps.cartocdn.com/light_all/{z}/{x}/{y}.png"
         
