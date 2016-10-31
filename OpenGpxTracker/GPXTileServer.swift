72c4778e32629d4cd4842920fe7d67f541e4f2f5
merlos <merlos@gmail.com>
2016-10-31 16:21:27 +0100

    removed commented references to MapQuest (bye,bye!)

--- OpenGpxTracker/GPXTileServer.swift
+++ OpenGpxTracker/GPXTileServer.swift
@@ -11,7 +11,6 @@ import Foundation
 enum GPXTileServer: Int {
     case apple
     case openStreetMap
-    //case MapQuest
     case openCycleMap
     //case AnotherMap
     case cartoDB
@@ -21,7 +20,6 @@ enum GPXTileServer: Int {
         switch self {
         case .apple: return "Apple Mapkit"
         case .openStreetMap: return "Open Street Map"
-        //case .MapQuest: return "MapQuest Open"
         case .openCycleMap: return "Open Cycle Maps"
         case .cartoDB: return "Carto DB"
         //case .AnotherMap: return "My Map"
@@ -32,7 +30,6 @@ enum GPXTileServer: Int {
         switch self {
         case .apple: return ""
         case .openStreetMap: return "http://tile.openstreetmap.org/{z}/{x}/{y}.png"
-        //case .MapQuest: return "http://otile3.mqcdn.com/tiles/1.0.0/osm/{z}/{x}/{y}.jpg"
         case .openCycleMap: return "http://b.tile.opencyclemap.org/cycle/{z}/{x}/{y}.png"
         case .cartoDB: return "http://b.basemaps.cartocdn.com/light_all/{z}/{x}/{y}.png"
         
