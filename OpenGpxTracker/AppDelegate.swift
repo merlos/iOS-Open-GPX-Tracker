e227514cc42db4088d66120cfe2c472817b5b404
merlos <merlos@gmail.com>
2018-09-22 14:22:58 -0400

    fixed bug that crashed the app before terminating.

--- OpenGpxTracker/AppDelegate.swift
+++ OpenGpxTracker/AppDelegate.swift
@@ -51,7 +51,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func applicationWillTerminate(_ application: UIApplication) {
         // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
         // Saves changes in the application's managed object context before the application terminates.
-        self.saveContext()
+        //self.saveContext()
     }
 
     func application(_ app: UIApplication, open url: URL, options: [UIApplicationOpenURLOptionsKey : Any] = [:]) -> Bool {
