2f80b6951b96740e11b2d00a05004155e4b30abe
Vincent Neo <23420208+vincentneo@users.noreply.github.com>
2019-02-05 19:29:29 +0800

    Adding watch target

---
+++ OpenGpxTracker-Watch Extension/InterfaceController.swift
@@ -0,0 +1,31 @@
+//
+//  InterfaceController.swift
+//  OpenGpxTracker-Watch Extension
+//
+//  Created by Vincent on 5/2/19.
+//  Copyright © 2019 TransitBox. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+
+
+class InterfaceController: WKInterfaceController {
+
+    override func awake(withContext context: Any?) {
+        super.awake(withContext: context)
+        
+        // Configure interface objects here.
+    }
+    
+    override func willActivate() {
+        // This method is called when watch view controller is about to be visible to user
+        super.willActivate()
+    }
+    
+    override func didDeactivate() {
+        // This method is called when watch view controller is no longer visible
+        super.didDeactivate()
+    }
+
+}
