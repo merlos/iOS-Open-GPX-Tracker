ad54af2878a4b1291fae6b44cea09a0f5fd1e58d
Vincent Neo <23420208+vincentneo@users.noreply.github.com>
2019-03-14 15:08:00 +0800

    Remove unused code in GPXFileTableRowController.swift

--- OpenGpxTracker-Watch Extension/GPXFileTableRowController.swift
+++ OpenGpxTracker-Watch Extension/GPXFileTableRowController.swift
@@ -14,27 +14,5 @@ class GPXFileTableRowController: NSObject {
     
     /// Label of cell
     @IBOutlet var fileLabel: WKInterfaceLabel!
-    /*
-    @IBOutlet var imageView: WKInterfaceImage!
     
-    
-    var lastFileName: String?
-    
-    func startSendIndicator(_ fileName: String?) {
-        imageView.setImageNamed("Progress-")
-        imageView.startAnimatingWithImages(in: NSMakeRange(1, 12), duration: 1, repeatCount: -1)
-        imageView.startAnimating()
-        imageView.setHidden(false)
-        lastFileName = fileName
-        fileLabel.setText("Sending File...")
-    }
-    
-    func stopSendIndicator() {
-        imageView.stopAnimating()
-        imageView.setHidden(true)
-        if let previousFileName = lastFileName {
-            fileLabel.setText(previousFileName)
-        }
-    }
- */
 }
