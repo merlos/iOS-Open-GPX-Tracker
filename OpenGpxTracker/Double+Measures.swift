504bfc574674eaec4f0b62ee6783c95e3bf979fc
Vincent Neo <23420208+vincentneo@users.noreply.github.com>
2019-05-09 22:12:49 +0800

    Documentation Improvements

--- OpenGpxTracker/Double+Measures.swift
+++ OpenGpxTracker/Double+Measures.swift
@@ -44,7 +44,7 @@ extension Double {
         return self/kMetersPerFeet
     }
     
-    /// Assuming current value is in meters, it returns
+    /// Assuming current value is in meters, it returns the equivalent string in feet without decimals and with "ft"
     func toFeet() -> String {
         return String(format: "%.0fft", self.toFeet() as Double)
     }
@@ -56,10 +56,11 @@ extension Double {
     
     /// Assuming current value is in meters, it returns the equivalent string
     /// in miles with two decimals and "mi"
-    /// For example:
     ///
-    ///      Double d = 1609.344
-    ///      d.toMilesString() => "1.00mi"
+    /// - Example:
+    ///
+    ///         Double d = 1609.344
+    ///         d.toMilesString() => "1.00mi"
     ///
     func toMiles() -> String {
         return String(format: "%.2fmi", toMiles() as Double)
@@ -72,12 +73,14 @@ extension Double {
     
     /// Assuming current value is in meters, it returns a string wiht the equivalent in
     /// kilometers with two decimals and km
+    ///
     /// Example: Current value is 1210.0, it returns "1.21km"
     func toKilometers() -> String {
         return String(format: "%.2fkm", toKilometers() as Double)
     }
     
     /// Returns current value as a string without decimals and with m.
+    ///
     /// Example: Current value is 1210.13, it returns "1210m"
     func toMeters() -> String {
         return String(format: "%.0fm", self)
@@ -96,48 +99,59 @@ extension Double {
     }
     
     /// Assuming current value is a speed in meters per second (m/s),
-    /// it returns the speed in miles per hour (mph)
+    ///
+    /// - Returns:
+    ///     The speed in miles per hour (mph)
     func toMilesPerHour() -> Double {
         return self * kMilesPerHourInOneMeterPerSecond
     }
     
     /// Assuming current value is a speed in meters per second (m/s),
-    /// it returns the speed in miles per hour (mph) with two decimals as
+    ///
+    /// - Returns:
+    ///     The speed in miles per hour (mph) with two decimals as
     /// string ("120.34mph")
     func toMilesPerHour() -> String {
         return String(format: "%.2fmph", toMilesPerHour() as Double)
     }
     
     /// Assuming current value is a speed in meters per second (m/s),
-    /// it returns the speed in kilometers per hour (km/h)
+    ///
+    /// - Returns:
+    ///     The speed in kilometers per hour (km/h)
     func toKilometersPerHour() -> Double {
         return self * kKilometersPerHourInOneMeterPerSecond
     }
     
     /// Assuming current value is a speed in meters per second (m/s),
-    /// it returns the speed in kilometers per hour with two decimals as
+    ///
+    /// - Returns:
+    ///     The speed in kilometers per hour with two decimals as
     /// string  ("120.34km/h")
     func toKilometersPerHour() -> String {
         return String(format: "%.2fkm/h", toKilometersPerHour() as Double)
     }
     
     /// Assuming current value is a speed in meters per second (m/s),
-    /// it returns the speed in km/h (100.00km/h)
-    /// or mph (60.00mph) if `useImperial` is set to `true`.
+    ///
+    /// - Returns:
+    ///     The speed in km/h (100.00km/h) or mph (60.00mph) if `useImperial` is set to `true`.
     func toSpeed(useImperial: Bool = false) -> String {
         return useImperial ? toMilesPerHour() : toKilometersPerHour() as String
     }
     
     /// Asuming current value is an altitud in meters,
-    /// it returns the altitude in m ("100m")
-    /// or in feet (304ft) if `useImperial` is set to `true`.
+    ///
+    /// - Returns:
+    ///     The altitude in m ("100m") or in feet (304ft) if `useImperial` is set to `true`.
     func toAltitude(useImperial: Bool = false) -> String {
         return useImperial ? toFeet() : toMeters() as String
     }
     
     /// Asuming current value is an altitud in meters,
-    /// it returns the altitude in m ("±100m")
-    /// or in feet (±304ft) if `useImperial` is set to `true`.
+    ///
+    /// - Returns:
+    ///     The altitude in m ("±100m") or in feet (±304ft) if `useImperial` is set to `true`.
     func toAccuracy(useImperial: Bool = false) -> String {
         return "±\(useImperial ? toFeet() as String : toMeters() as String)"
     }
