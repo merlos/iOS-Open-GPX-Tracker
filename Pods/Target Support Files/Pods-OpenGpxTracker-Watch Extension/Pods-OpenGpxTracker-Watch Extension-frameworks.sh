a4d0cc6f7fc0c4746048d168179bd6ecdac78772
merlos <merlos@users.noreply.github.com>
2019-09-01 21:51:30 -0400

    remove CryptoSwift as it was only used by Cache

--- Pods/Target Support Files/Pods-OpenGpxTracker-Watch Extension/Pods-OpenGpxTracker-Watch Extension-frameworks.sh
+++ Pods/Target Support Files/Pods-OpenGpxTracker-Watch Extension/Pods-OpenGpxTracker-Watch Extension-frameworks.sh
@@ -162,11 +162,9 @@ strip_invalid_archs() {
 
 if [[ "$CONFIGURATION" == "Debug" ]]; then
   install_framework "${BUILT_PRODUCTS_DIR}/CoreGPX-watchOS/CoreGPX.framework"
-  install_framework "${BUILT_PRODUCTS_DIR}/CryptoSwift-watchOS/CryptoSwift.framework"
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
   install_framework "${BUILT_PRODUCTS_DIR}/CoreGPX-watchOS/CoreGPX.framework"
-  install_framework "${BUILT_PRODUCTS_DIR}/CryptoSwift-watchOS/CryptoSwift.framework"
 fi
 if [ "${COCOAPODS_PARALLEL_CODE_SIGN}" == "true" ]; then
   wait
