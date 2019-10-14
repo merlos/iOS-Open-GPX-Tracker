f5cd7cb6eaa5e963b3c3220c2a623dfedfa03c55
Vincent Neo <23420208+vincentneo@users.noreply.github.com>
2019-10-14 22:32:17 +0800

    fix #132; update CoreGPX -> 0.7.1

--- Pods/CoreGPX/Classes/Converters.swift
+++ Pods/CoreGPX/Classes/Converters.swift
@@ -52,6 +52,7 @@ internal final class Convert {
         
         // Timezone should be 0 as GPX uses UTC time, not local time.
         formatter.timeZone = TimeZone(secondsFromGMT: 0)
+        formatter.locale = Locale(identifier: "en_US_POSIX")
         
         // dateTime（YYYY-MM-DDThh:mm:ssZ）
         formatter.dateFormat = "yyyy'-'MM'-'dd'T'HH':'mm':'ss'Z'"
