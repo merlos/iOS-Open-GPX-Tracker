b4668e5277c31e211e2dd9446f3502caba3d0b07
merlos <merlos@users.noreply.github.com>
2020-05-03 20:27:42 -0400

    prevent out of range error if a preset is removed in the future

--- OpenGpxTracker/Preferences.swift
+++ OpenGpxTracker/Preferences.swift
@@ -257,7 +257,7 @@ class Preferences: NSObject {
     var dateFormatPresetName: String {
         get {
             let presets =  ["Defaults", "ISO8601 (UTC)", "ISO8601 (UTC offset)", "Day, Date at time (12 hr)", "Day, Date at time (24 hr)"]
-            return presets[_dateFormatPreset]
+            return _dateFormatPreset < presets.count ? presets[_dateFormatPreset] : "???"
         }
     }
     
