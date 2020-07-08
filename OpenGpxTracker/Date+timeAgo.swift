ce8a8e611d7c3cec53b2c416606b9fb19d6b8b2f
Bram Huenaerts <bram@chronux.be>
2020-07-08 21:52:40 +0200

    fixup! Fix swift lint build warnings

--- OpenGpxTracker/Date+timeAgo.swift
+++ OpenGpxTracker/Date+timeAgo.swift
@@ -32,49 +32,49 @@ extension Date {
         //print("")
         //print(components)
         if let year = components.year {
-            if (year >= 2) {
+            if year >= 2 {
                 return String(format: NSLocalizedString("T_YEARS_AGO", comment: "no comment"), year)
-            } else if (year >= 1) {
+            } else if year >= 1 {
                 return numericDates ? NSLocalizedString("T_YEAR_AGO", comment: "no comment") : NSLocalizedString("T_LAST_YEAR", comment: "no comment")
             }
         }
         if let month = components.month {
-            if (month >= 2) {
+            if month >= 2 {
                 return String(format: NSLocalizedString("T_MONTHS_AGO", comment: "no comment"), month)
-            } else if (month >= 1) {
+            } else if month >= 1 {
                 return numericDates ? NSLocalizedString("T_MONTH_AGO", comment: "no comment") : NSLocalizedString("T_LAST_MONTH", comment: "no comment")
             }
         }
         if let weekOfMonth = components.weekOfMonth {
-            if (weekOfMonth >= 2) {
+            if weekOfMonth >= 2 {
                 return String(format: NSLocalizedString("T_WEEKS_AGO", comment: "no comment"), weekOfMonth)
-            } else if (weekOfMonth >= 1) {
+            } else if weekOfMonth >= 1 {
                 return numericDates ? NSLocalizedString("T_WEEK_AGO", comment: "no comment") : NSLocalizedString("T_LAST_WEEK", comment: "no comment")
             }
         }
         if let day = components.day {
-            if (day >= 2) {
+            if day >= 2 {
                 return String(format: NSLocalizedString("T_DAYS_AGO", comment: "no comment"), day)
-            } else if (day >= 1) {
+            } else if day >= 1 {
                 return numericDates ? NSLocalizedString("T_DAY_AGO", comment: "no comment") : NSLocalizedString("T_YESTERDAY", comment: "no comment")
             }
         }
         if let hour = components.hour {
-            if (hour >= 2) {
+            if hour >= 2 {
                 return String(format: NSLocalizedString("T_HOURS_AGO", comment: "no comment"), hour)
-            } else if (hour >= 1) {
+            } else if hour >= 1 {
                 return numericDates ? NSLocalizedString("T_HOUR_AGO", comment: "no comment") : NSLocalizedString("T_LAST_HOUR", comment: "no comment")
             }
         }
         if let minute = components.minute {
-            if (minute >= 2) {
+            if minute >= 2 {
                 return String(format: NSLocalizedString("T_MINUTES_AGO", comment: "no comment"), minute)
-            } else if (minute >= 1) {
+            } else if minute >= 1 {
                 return numericDates ? NSLocalizedString("T_MINUTE_AGO", comment: "no comment") : NSLocalizedString("T_LAST_MINUTE", comment: "no comment")
             }
         }
         if let second = components.second {
-            if (second >= 3) {
+            if second >= 3 {
                 return String(format: NSLocalizedString("T_SECONDS_AGO", comment: "no comment"), second)
             }
         }
