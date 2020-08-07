e29d83b687630511b67f49bfb5d72b5cef58a416
Alan Heezen <aheezen@emich.edu>
2020-08-07 09:59:13 -0700

    // Should be ready for a Pull request now

--- OpenGpxTracker/HTTrack.swift
+++ OpenGpxTracker/HTTrack.swift
@@ -9,6 +9,7 @@ import Foundation
 import CoreLocation
 
 // An implementation of SmoothTrack.swift to be merged into Open GPX Tracker
+// Should be ready for a Pull request now
 
 class HTTrack {
     
